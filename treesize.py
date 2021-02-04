0e88d9b8bb8ede2db7a2c0caef24d3a8242710a5
arhonemus <34036628+arhonemus@users.noreply.github.com>
2021-02-04 15:21:45 -0500

    treesize

---
+++ treesize.py
@@ -0,0 +1,30 @@
+import os
+
+def subdirs(path):
+    """Yield directory names not starting with '.' under given path."""
+    for entry in os.scandir(path):
+        if not entry.name.startswith('.') and entry.is_dir():
+            yield entry.name
+            print (entry.name)
+def get_tree_size(path):
+    """Return total size of files in path and subdirs. If
+    is_dir() or stat() fails, print an error message to stderr
+    and assume zero size (for example, file has been deleted).
+    """
+    total = 0
+    for entry in os.scandir(path):
+        try:
+            is_dir = entry.is_dir(follow_symlinks=False)
+        except OSError as error:
+            print('Error calling is_dir():', error, file=sys.stderr)
+            continue
+        if is_dir:
+            total += get_tree_size(entry.path)
+            print(entry.path, get_tree_size(entry.path))
+        else:
+            try:
+                total += entry.stat(follow_symlinks=False).st_size
+            except OSError as error:
+                print('Error calling stat():', error, file=sys.stderr)
+    return total
+get_tree_size(path='/Volumes/lpasync/9_not_MIable/Staged_BornDigital_Excel/2013_224_silversodfy1311')
